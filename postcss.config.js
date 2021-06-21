1073ecce41cd178f9fa491d82ffc0b4c81274ad2
hiro <hiro0218@gmail.com>
2021-06-21 17:49:27 +0900

    update: バージョンコメントを動的に変更する

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,12 @@
+const package = require("./package.json");
+
 module.exports = (ctx) => ({
   plugins: {
     cssnano: ctx.env === "production" ? { preset: "default" } : false,
+    "postcss-banner": {
+      banner: `${package.name} v${package.version} | ${package.license} License | github.com/hiro0218/shokika.css/`,
+      inline: true,
+      important: true,
+    },
   },
 });
