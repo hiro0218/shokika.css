d015fd6976d7f66793d45b2668b813b569ec5516
hiroyuki kikuchi <hiroyuki.kikuchi@zozo.com>
2022-07-04 19:50:37 +0900

    fix: style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,7 +30,7 @@ const Home = () => {
         <meta name="twitter:creator" content="@hiro0218" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap" rel="stylesheet" />
       </Head>
       <TheHeader />
       <TheMain />
