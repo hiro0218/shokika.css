122fa3291db269a776d9c719feb564fc65577d71
hiro <hiro0218@gmail.com>
2025-07-04 21:45:54 +0900

    chore: remove stylesheet link from index.html and adjust import in main.tsx

--- docs/main.tsx
+++ docs/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 
 import './assets/css/docs.css';
+import '../src/shokika.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
